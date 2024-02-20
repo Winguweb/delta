d59f11f6c598d2a72bfc285afe0d090a338d27fc
valentinarau <vraurobayo@frba.utn.edu.ar>
2024-02-19 21:55:32 -0300

    Add authentication for 'AppNotifier' API call

--- server/notifier/appNotifierService.ts
+++ server/notifier/appNotifierService.ts
@@ -2,14 +2,30 @@ import axios from 'axios';
 import { DeviceCoordinates } from './deviceCoordinates';
 
 const http = axios.create({
-  baseURL: process.env.NOTIFIER_URL
+  baseURL: process.env.NOTIFIER_URL,
 });
 
 export async function onSampleUpload(coordinates: DeviceCoordinates) {
   try {
-    const res = await http.post('/onUpdate', coordinates);
+    const token = await authenticate();
+    const headers = { 'Authorization': 'Bearer ' + token };
+    const res = await http.post(
+      '/onUpdate',
+      coordinates,
+      { headers: headers },
+    );
     return res.data;
-  } catch (err) {
-    console.error(err);
+  } catch (err: any) {
+    console.error('[NotifierService] error:', err.response.data.error);
+  }
+}
+
+async function authenticate() {
+  try {
+    const body = { privateKey: process.env.NOTIFIER_SECRET };
+    const res = await http.post('/auth', body);
+    return res.data.token;
+  } catch (err: any) {
+    console.error('[NotifierService][Auth] error:', err.response.data.error);
   }
 }
